2581e363a9cfd3f828aa2c20c6b87da14dc9c930
IncreaseOmoyin71 <increase.omoyin@ploutosfinance.com>
2026-04-16 14:42:55 +0100

    Hangfire

--- Feex.Hangfire/Program.cs
+++ Feex.Hangfire/Program.cs
@@ -1,57 +1,55 @@
 using Hangfire;
-using Microsoft.Extensions.Configuration;
-using Feex.Application;
-//using Feex.Application.Services;
-//using Feex.Hangfire;
-using System.Text.Encodings.Web;
+using Hangfire.SqlServer;
+using Microsoft.Extensions.Logging;
 using System.Text.Json.Serialization;
+using System.Text.Encodings.Web;
 using System.Text.Unicode;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// -------------------- LOGGING --------------------
 builder.Logging.ClearProviders();
 builder.Logging.AddConsole();
 builder.Logging.SetMinimumLevel(LogLevel.Information);
 
+// -------------------- CONTROLLERS --------------------
 builder.Services.AddControllers()
     .AddJsonOptions(o =>
     {
         o.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
         o.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
         o.JsonSerializerOptions.Encoder = JavaScriptEncoder.Create(UnicodeRanges.All);
-    });// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    });
+
 builder.Services.AddEndpointsApiExplorer();
+
+// -------------------- HANGFIRE --------------------
 builder.Services.AddHangfire(config =>
 {
-    config.UseSqlServerStorage(builder.Configuration.GetConnectionString("HangfireContext"));
+    config.UseSqlServerStorage(
+        builder.Configuration.GetConnectionString("HangfireConnection")
+    );
 });
-//builder.Services.AddHangfireServer();
-//builder.RegisterCustomServices(builder.Configuration);
-//builder.Services.AddScoped<JobService>();
-//builder.Services.AddScoped<PaystackProcessorService>();
+
+builder.Services.AddHangfireServer(); // ?? REQUIRED
 
 var app = builder.Build();
-using (var scope = app.Services.CreateScope())
-{
-    var services = scope.ServiceProvider;
-    //var hangfireJobService = services.GetRequiredService<JobService>();
-    //hangfireJobService.ScheduleRecurringJobs();
-}
 
-// Configure the HTTP request pipeline.
+// -------------------- PIPELINE --------------------
+
+// Dashboard FIRST
+app.UseHangfireDashboard("/hangfire");
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
 
 app.MapControllers();
 
-app.UseHangfireDashboard("/hangfire");
-app.UseHangfireServer();
-
-app.MapGet("/swagger/index.html", () => Results.Redirect("/hangfire"));
-app.MapGet("/swagger", () => Results.Redirect("/hangfire"));
-app.MapGet("/", () => Results.Redirect("/hangfire"));
+// -------------------- TEST JOB --------------------
+RecurringJob.AddOrUpdate(
+    "hangfire-test-job",
+    () => Console.WriteLine("?? Hangfire is working"),
+    "* * * * *"
+);
 
 app.Run();
